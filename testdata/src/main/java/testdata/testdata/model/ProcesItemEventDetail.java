9efc7f42d9bf824979cca15c57a44cb968902dda
gurmaj <gurmaj.singh@gmail.com>
2014-09-08 21:18:35 +0100

    Created ProcessItem Event Detail

---
+++ testdata/src/main/java/testdata/testdata/model/ProcesItemEventDetail.java
@@ -0,0 +1,127 @@
+package testdata.testdata.model;
+
+import java.util.Date;
+
+public class ProcesItemEventDetail {
+	
+	Date timestamp;
+	double longitude;
+	double latitude;
+	String unitCode;
+	String uniqueItemId;
+	short weightGrams;
+	short declaredWeightGrams;
+	short lenghMillimetres;
+	short widthMillimetres;
+	short heighMillimetres;
+	short declaredLenghMillimetres;
+	short declaredWidthMillimetres;
+	short declaredHeighMillimetres;	
+	String shapeType;
+	String destinationPostcode;
+	String sourcePostCode;
+	String customerAccountNumber;
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public String getUnitCode() {
+		return unitCode;
+	}
+	public void setUnitCode(String unitCode) {
+		this.unitCode = unitCode;
+	}
+	public String getUniqueItemId() {
+		return uniqueItemId;
+	}
+	public void setUniqueItemId(String uniqueItemId) {
+		this.uniqueItemId = uniqueItemId;
+	}
+	public short getWeightGrams() {
+		return weightGrams;
+	}
+	public void setWeightGrams(short weightGrams) {
+		this.weightGrams = weightGrams;
+	}
+	public short getDeclaredWeightGrams() {
+		return declaredWeightGrams;
+	}
+	public void setDeclaredWeightGrams(short declaredWeightGrams) {
+		this.declaredWeightGrams = declaredWeightGrams;
+	}
+	public short getLenghMillimetres() {
+		return lenghMillimetres;
+	}
+	public void setLenghMillimetres(short lenghMillimetres) {
+		this.lenghMillimetres = lenghMillimetres;
+	}
+	public short getWidthMillimetres() {
+		return widthMillimetres;
+	}
+	public void setWidthMillimetres(short widthMillimetres) {
+		this.widthMillimetres = widthMillimetres;
+	}
+	public short getHeighMillimetres() {
+		return heighMillimetres;
+	}
+	public void setHeighMillimetres(short heighMillimetres) {
+		this.heighMillimetres = heighMillimetres;
+	}
+	public short getDeclaredLenghMillimetres() {
+		return declaredLenghMillimetres;
+	}
+	public void setDeclaredLenghMillimetres(short declaredLenghMillimetres) {
+		this.declaredLenghMillimetres = declaredLenghMillimetres;
+	}
+	public short getDeclaredWidthMillimetres() {
+		return declaredWidthMillimetres;
+	}
+	public void setDeclaredWidthMillimetres(short declaredWidthMillimetres) {
+		this.declaredWidthMillimetres = declaredWidthMillimetres;
+	}
+	public short getDeclaredHeighMillimetres() {
+		return declaredHeighMillimetres;
+	}
+	public void setDeclaredHeighMillimetres(short declaredHeighMillimetres) {
+		this.declaredHeighMillimetres = declaredHeighMillimetres;
+	}
+	public String getShapeType() {
+		return shapeType;
+	}
+	public void setShapeType(String shapeType) {
+		this.shapeType = shapeType;
+	}
+	public String getDestinationPostcode() {
+		return destinationPostcode;
+	}
+	public void setDestinationPostcode(String destinationPostcode) {
+		this.destinationPostcode = destinationPostcode;
+	}
+	public String getSourcePostCode() {
+		return sourcePostCode;
+	}
+	public void setSourcePostCode(String sourcePostCode) {
+		this.sourcePostCode = sourcePostCode;
+	}
+	public String getCustomerAccountNumber() {
+		return customerAccountNumber;
+	}
+	public void setCustomerAccountNumber(String customerAccountNumber) {
+		this.customerAccountNumber = customerAccountNumber;
+	}
+}
